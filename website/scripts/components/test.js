c74ad5a76184124e59c3417de5e461f2f414c560
Yining-lion <136415430+Yining-lion@users.noreply.github.com>
2024-06-28 17:02:12 +0800

    Delete website directory

--- website/scripts/components/test.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".size").forEach(productsSize => {
-    productsSize.addEventListener("click", () => {
-        productsSize.style = "border: 1px black solid"
-        const productSize = productsSize.dataset.size;
-        document.querySelector(".product_name").innerHTML = `${product.name} ( ${productSize} )`
-    });
-});
--- website/scripts/pages/checkout.js
+++
@@ -1,7 +0,0 @@
-import { renderHeader } from "../components/header.js";
-import { renderCheckoutOder } from "../components/checkout_oder.js";
-import { renderCheckoutPrice } from "../components/checkout_price.js";
-
-renderHeader();
-renderCheckoutOder();
-renderCheckoutPrice();
