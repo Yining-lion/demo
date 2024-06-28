d7f1ba8df2cc20bdcf3ae17b34429671833864d6
Yining-lion <136415430+Yining-lion@users.noreply.github.com>
2024-06-28 10:58:17 +0800

    Delete website directory

--- website/scripts/pages/search.js
+++
@@ -1,9 +0,0 @@
-import { renderHeader } from "../components/header.js";
-import { searchProduct } from "../../data/products.js";
-import { renderProductGrid } from "../components/product_grid.js";
-
-renderHeader();
-
-const params = new URLSearchParams(location.search);
-const products = searchProduct(params.get("keyWord"));
-renderProductGrid(products);
