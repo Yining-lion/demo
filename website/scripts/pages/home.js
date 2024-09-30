31cf68d5f1d4b6e1eb37270fd488920aad7cfd7f
Yining-lion <136415430+Yining-lion@users.noreply.github.com>
2024-09-30 14:25:06 +0800

    Delete website directory

--- website/scripts/pages/home.js
+++
@@ -1,6 +0,0 @@
-import { products } from "../../data/products.js";
-import { renderHeader } from "../components/header.js";
-import { renderProductGrid } from "../components/product_grid.js";
-
-renderHeader();
-renderProductGrid(products);
--- website/scripts/pages/product.js
+++
@@ -1,7 +0,0 @@
-import { renderHeader } from "../components/header.js";
-import { renderProductInfo } from "../components/product_info.js";
-import { renderProductFunction } from "../components/product_function.js";
-
-renderHeader();
-renderProductInfo();
-renderProductFunction();
